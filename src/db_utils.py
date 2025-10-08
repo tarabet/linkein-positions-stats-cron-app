20f56915974fa11d17958dbbce40be33d27c2e7c
tarabet <taranenko.alexander@gmil.com>
2025-10-08 21:26:35 +0200

    rewritten to use neon postgress db

--- src/db_utils.py
+++ src/db_utils.py
@@ -1,28 +1,66 @@
-def check_db(con):
-    table_name: str = "STATS"
+from sqlalchemy import create_engine, Column, Integer, Text, TIMESTAMP, Sequence
+from sqlalchemy.ext.declarative import declarative_base
+from loguru import logger as log
 
-    fields_list: list[str] = [
-        "ID INTEGER PRIMARY KEY AUTOINCREMENT",
-        "DATE INT NOT NULL",
-        "TECH TEXT NOT NULL",
-        "ANY_TIME INT",
-        "PAST_MONTH INT",
-        "PAST_WEEK INT",
-        "PAST_24_HOURS INT",
-        "FULL_TIME INT",
-        "PART_TIME INT",
-        "CONTRACT INT",
-        "TEMPORARY INT",
-        "VOLUNTEER INT",
-        "INTERNSHIP INT",
-        "ENTRY_LEVEL INT",
-        "ASSOCIATE INT",
-        "MID_SENIOR INT",
-        "DIRECTOR INT",
-        "ON_SITE INT",
-        "HYBRID INT",
-        "REMOTE INT"
-    ]
+Base = declarative_base()
 
-    con.execute(f"CREATE TABLE IF NOT EXISTS {table_name}({','.join(map(str, fields_list))});")
-    con.commit()
+class TechStats(Base):
+    __tablename__ = "stats"
+
+    id = Column(Integer, Sequence("user_id_seq"), primary_key=True, autoincrement=True)
+    date = Column(TIMESTAMP)
+    tech = Column(Text)
+    any_time = Column(Integer)
+    past_month = Column(Integer)
+    past_week = Column(Integer)
+    past_24_hours = Column(Integer)
+    full_time = Column(Integer)
+    part_time = Column(Integer)
+    contract = Column(Integer)
+    temporary = Column(Integer)
+    internship = Column(Integer)
+    entry_level = Column(Integer)
+    associate = Column(Integer)
+    mid_senior = Column(Integer)
+    director = Column(Integer)
+    on_site = Column(Integer)
+    hybrid = Column(Integer)
+    remote = Column(Integer)
+
+def db_insert(technology_stats_object, db_url):
+    try:
+        engine = create_engine(db_url)
+        Base.metadata.create_all(engine)
+
+        from sqlalchemy.orm import Session
+
+        log.debug(f"Storing tech stats to database")
+
+        with Session(engine) as session:
+        # Create technology stats record
+            tech_stats_entity = TechStats(
+                date=technology_stats_object['DATE'],
+                tech=technology_stats_object['TECH'],
+                any_time=technology_stats_object['ANY_TIME'],
+                past_month=technology_stats_object['PAST_MONTH'],
+                past_week=technology_stats_object['PAST_WEEK'],
+                past_24_hours=technology_stats_object['PAST_24_HOURS'],
+                full_time=technology_stats_object['FULL_TIME'],
+                part_time=technology_stats_object['PART_TIME'],
+                contract=technology_stats_object['CONTRACT'],
+                temporary=technology_stats_object['TEMPORARY'],
+                internship=technology_stats_object['INTERNSHIP'],
+                entry_level=technology_stats_object['ENTRY_LEVEL'],
+                associate=technology_stats_object['ASSOCIATE'],
+                mid_senior=technology_stats_object['MID_SENIOR'],
+                director=technology_stats_object['DIRECTOR'],
+                on_site=technology_stats_object['ON_SITE'],
+                hybrid=technology_stats_object['HYBRID'],
+                remote=technology_stats_object['REMOTE'],
+            )
+
+        session.add(tech_stats_entity)
+        session.commit()
+
+    except Exception as e:
+        log.error("DB save error:", e)
--- src/main.py
+++ src/main.py
@@ -2,27 +2,18 @@ import requests
 import os
 import urllib.parse
 
+from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 from labels_mapper import labels_mapper
+from src.db_utils import db_insert
 from tech_list import tech_list
 from random import randint
 from time import sleep
-import sqlite3
 import datetime
-from db_utils import check_db
+from loguru import logger as log
 
-db_url = os.environ.get("DB_URL")
-con = sqlite3.connect(db_url)
-cur = con.cursor()
+load_dotenv()
 
-def db_insert(obj):
-    print("OBJECT:", obj)
-    table_name: str = "STATS"
-    placeholders = ', '.join(['?'] * len(obj))
-    columns = ', '.join(obj.keys())
-    sql = "INSERT INTO %s ( %s ) VALUES ( %s )" % (table_name, columns, placeholders)
-
-    cur.execute(sql, list(obj.values()))
 
 def get_data_by_label_id(soup, label_id: str) -> str:
     return soup.find("label", {"for": label_id}).getText().split('(', 1)[1].split(')')[0]
@@ -53,32 +44,28 @@ def get_page_content(tech: str):
         except:
             stats_obj[key] = None
 
-    # stats_obj = {'DATE': '2025-01-26', 'TECH': 'Python', 'ANY_TIME': 14014, 'PAST_MONTH': 8950, 'PAST_WEEK': 2319, 'PAST_24_HOURS': 110, 'FULL_TIME': 12584, 'PART_TIME': 416, 'CONTRACT': 416, 'TEMPORARY': 53, 'VOLUNTEER': 1, 'INTERNSHIP': 2227, 'ENTRY_LEVEL': 3712, 'ASSOCIATE': 1390, 'MID_SENIOR': 5110, 'DIRECTOR': 112, 'ON_SITE': 7005, 'HYBRID': 4600, 'REMOTE': 2065}
+    # stats_obj = {'DATE': '2025-08-24 21:03:43.436971', 'TECH': 'Python', 'ANY_TIME': 14014, 'PAST_MONTH': 8950, 'PAST_WEEK': 2319, 'PAST_24_HOURS': 110, 'FULL_TIME': 12584, 'PART_TIME': 416, 'CONTRACT': 416, 'TEMPORARY': 53, 'VOLUNTEER': 1, 'INTERNSHIP': 2227, 'ENTRY_LEVEL': 3712, 'ASSOCIATE': 1390, 'MID_SENIOR': 5110, 'DIRECTOR': 112, 'ON_SITE': 7005, 'HYBRID': 4600, 'REMOTE': 2065}
 
-    db_insert(stats_obj)
+    return stats_obj
 
 def main():
     env = os.environ.get("ENV")
-    bot_token = os.environ.get("BOT_TOKEN")
-    bot_chat_id = os.environ.get("BOT_CHAT_ID")
-
+    db_url = os.getenv("DB_URL", env)
+    bot_token = os.getenv("BOT_TOKEN", env)
+    bot_chat_id = os.getenv("BOT_CHAT_ID", env)
     bot_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
 
-    print(f"Running in {env} mode")
-    print(f"Db Url:", db_url)
-
-    check_db(con)
+    log.info(f"Running in {env} mode")
 
     try:
         for i, tech in enumerate(tech_list):
             rand_time = randint(10, 30)
             sleep(rand_time)
-            get_page_content(tech)
+            technology_stats_object = get_page_content(tech)
+            db_insert(technology_stats_object, db_url)
 
             if i == len(tech_list) - 1:
                 job_finished_time = datetime.datetime.today().isoformat()
-                con.commit()
-
                 response = requests.get(bot_url, params={"chat_id": bot_chat_id, "text": f"All positions scraped on {job_finished_time}. Saved in {db_url} database."})
 
                 if response.status_code == 200:
@@ -89,9 +76,9 @@ def main():
         response = requests.get(bot_url, params={"chat_id": bot_chat_id, "text": f"Error happened: {e}"})
 
         if response.status_code == 200:
-            print("Message sent successfully!")
+            print("Error Message sent successfully!")
         else:
-            print("Failed to send message:", response.text)
+            print("Error message send Failed:", response.text)
 
 if __name__ == "__main__":
     main()
